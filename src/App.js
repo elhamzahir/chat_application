67ae634236daebc8e5d56f2fc85f148b31bd4bd9
Elham_Zahir <elham.zahirk@gmail.com>
2022-12-19 19:39:07 +0330

    add person

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <ChatEngine
+          height="100vh"
+          projectID="153e743d-6180-4a26-a3e6-41c967252218"
+          userName="elham"
+          userSecret="elham@1381"
+      />
     </div>
   );
 }
